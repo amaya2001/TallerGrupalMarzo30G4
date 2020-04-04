bbb3f0d8c67d219950205c1782df0a9984192c35
Juanfe Penagos <juanfelipeca1@outlook.com>
2020-04-04 15:12:13 -0500

    Todo funciona. Espacios Resueltos.

--- bsangre.c
+++ bsangre.c
@@ -1,141 +1,112 @@
-//FELIPE----------------------
 #include "bsangre.h"
 
-void donarSangre(  Usuario lista[ MAX ][ MAX ] ){
-    int aDonar = 0, i, j, aprov = 0;
-    char buscar[ 30 ];
+int numTipo( Usuario tmpUsuario ){
+	if ( strcmp( "A", tmpUsuario.tipo )  == 0 ){
+		return 0;
+    	} if ( strcmp( "B", tmpUsuario.tipo )  == 0 ){
+        	return 1;
+    	} if ( strcmp( "O", tmpUsuario.tipo )  == 0 ){
+        	return 2;
+    	} if ( strcmp( "AB", tmpUsuario.tipo )  == 0 ){
+        	return 3;
+    	}
+}
 
-    printf( "Ingrese el nombre del donante.\n" );
-    printf( "\n" );
-    fflush( stdin );
-    scanf( "%s" , &buscar );
-    for( i=0; i < MAX; i++ ){
-        for( j = 0; j < MAX; j++ ){
-            if( strcmp( buscar, lista[ i ][ j ].nombre ) == 0 ){
-                printf( "Se aceptan donaciones entre 350 y 550 ml.\n" );
-                printf( "Ingrese la sangre que desea donar en mililitros: " );
-                fflush( stdin );
-                scanf( "%d", &aDonar );
-                if( aDonar >= 350 && aDonar <= 550 ){
-                    lista[ i ][ j ].cantidad = aDonar;
-                } else{
-                    printf( "La cantidad de sangre a donar no es valida.\n" );
-                }
-                aprov++;
-            }
-        }
-    } if( aprov == 0 ){
-        printf( "El usuario ingresado no existe\n" );
-    }
+void guardarNuevoUsuario( Usuario lista[][MAX], Usuario * pUsuario, int registro[4], int i ){
+    	lista[i][registro[i]] = *pUsuario;
+}
+
+void agregarUsuario( Usuario lista[MAX][MAX], int registro[4] ){
+	Usuario tmpUsuario;
+	printf( "Ingrese el tipo de sangre: " );
+	fflush( stdin );
+	scanf( "%s" , &tmpUsuario.tipo );
+	int i;
+	i = numTipo( tmpUsuario );
+	if( registro[i]<4 ){
+		printf( "Ingrese el nombre: " );
+		fflush( stdin );
+		scanf( "%s" , &tmpUsuario.nombre );
+		tmpUsuario.cantidad = 0;
+		guardarNuevoUsuario( lista , &tmpUsuario , registro , i );
+		registro[i]++;
+	} else{
+		printf( "Lo sentimos. Ya no hay registros disponibles\n" );
+	}
+}
+
+void donarSangre( Usuario lista[ MAX ][ MAX ] ){
+	int aDonar = 0, i, j, aprov = 0;
+	char buscar[30];
+	printf( "Ingrese el nombre del donante:\n" );
+	fflush( stdin );
+	scanf( "%s" , &buscar );
+	for( i=0;i<MAX;i++ ){
+		for( j=0;j<MAX;j++ ){
+			if( strcmp( buscar, lista[i][j].nombre ) == 0 ){
+				printf( "Se aceptan donaciones entre 350 y 550 ml.\n" );
+                		printf( "Ingrese la sangre que desea donar en mililitros: " );
+                		fflush( stdin );
+                		scanf( "%d", &aDonar );
+                		if( aDonar >= 350 && aDonar <= 550 ){
+                    			lista[i][j].cantidad = aDonar;
+				} else{
+                    			printf( "La cantidad de sangre a donar no es valida.\n" );
+				} aprov++;
+                	}
+		}
+        } if( aprov == 0 ){
+		printf( "El usuario ingresado no existe\n" );
+	}
 }   
 
-int numTipo( Usuario* pUsuario ){
-    if ( strcmp( "A", pUsuario->tipo )  == 0 ){
-        return 0;
-    }
-    if ( strcmp( "B", pUsuario->tipo )  == 0 ){
-        return 1;
-    }   
-    if ( strcmp( "O", pUsuario->tipo )  == 0 ){
-        return 2;
-    }
-    if ( strcmp( "AB", pUsuario->tipo )  == 0 ){
-        return 3;
-    }
-}
-//----------------------------------------------
-void mostrarUsuario( Usuario lista[ MAX ][ MAX ] ){
-    char buscar[ 30 ];
-    int i, j, aprov = 0;
-
-    printf( "Ingrese el nombre del usuario que desea buscar: " );
-    printf( "\n" );
-    fflush( stdin );
-    scanf( "%s", &buscar );
-    for( i=0;i<MAX;i++ ){
-        for( j=0;j<MAX;j++ ){
-            if( strcmp( buscar , lista[ i ][ j ].nombre ) == 0 ){
-                printf( "El usario que usted ha buscado es:\n" );
-                printf( "[ %s, %s, %d ]\n", lista[ i ][ j ].nombre , lista[ i ][ j ].tipo , lista[ i ][ j ].cantidad );
-                aprov++;
-            }
-        }
-    } if( aprov == 0 ){
-        printf( "El usuario ingresado no existe\n" );
-    }
+void mostrarUsuario( Usuario lista[MAX][MAX] ){
+	char buscar[30];
+	int i, j, aprov = 0;
+	printf( "Ingrese el nombre del usuario que desea buscar: " );
+	fflush( stdin );
+	scanf( "%s", &buscar );
+	for( i=0;i<MAX;i++ ){
+        	for( j=0;j<MAX;j++ ){
+        		if( strcmp( buscar , lista[i][j].nombre ) == 0 ){
+                		printf( "El usario que usted ha buscado es:\n" );
+                		printf( "[ %s, %s, %d ]\n", lista[ i ][ j ].nombre , lista[ i ][ j ].tipo , lista[i][j].cantidad );
+                		aprov++;
+            		}
+        	}
+    	} if( aprov == 0 ){
+        	printf( "El usuario ingresado no existe\n" );
+    	}
 }   
-//SANTIAGO-----------------------
-void nuevoUsuario( Usuario lista[  ][ MAX ], Usuario* pUsuario ){
-    int i;
 
-    printf( "Ingrese el tipo de sangre: " );
-    fflush( stdin );
-    fgets( pUsuario -> tipo, 3, stdin );
-    printf( "\n" );
-    i = numTipo( pUsuario );
-    if (verificarEspacio( lista, i ) == 1 ){
-        printf( "Ingrese el nombre del individuo: " );
-        fflush( stdin );
-        fgets( pUsuario -> nombre, 30, stdin );
-        printf("\n");
-        pUsuario->cantidad = 0;
-        guardarNuevoUsuario( lista, pUsuario, i );
-    } else{
-        printf( "Lo sentimos. Ya no hay registros disponibles\n" );
-    }
-
-}
-
-int verificarEspacio(Usuario lista[  ][ MAX ], int i){
-    int j;
-
-    for( j = 0; j < MAX; j++ ){
-        if( lista[ i ][ j ].nombre[ 0 ] == '\0' ){
-            return 1;
-        }
-    } return 0;
-}
-
-void guardarNuevoUsuario( Usuario lista[  ][ MAX ], Usuario* pUsuario, int i ){
-    int j;
-
-    for( j = 0; j < MAX; j++ ){
-        if( lista[ i ][ j ].nombre == '\0' ){
-            lista[ i ][ j ] = *pUsuario;
-        }
-    }
-}
-//--------------------------
 void mostrarMenu(){
-    int opc;
-    Usuario * pUsuario;
-    Usuario lista[ MAX ][ MAX ];
-
-    do{
-        printf( "\nBienvenido al Banco de Sangre!\n" );
-        printf( "Por favor, eliga su opcion\n" );
-        printf( "1. Ingresar un usuario.\n" );
-        printf( "2. Donar sangre.\n" );
-        printf( "3. Mostrar donante.\n" );
-        printf( "0. Salir.\n" );
-        printf( "Su opcion: " );
-        printf( "\n" );
-        fflush( stdin );
-        scanf( "%d" , &opc );
-        switch( opc ){
-            case 0:
-                break;
-            case 1:
-                nuevoUsuario( lista , pUsuario );
-                break;
-            case 2:
-                donarSangre( lista );
-                break;
-            case 3:
-                mostrarUsuario( lista );
-                break;
-            default:
-                printf( "La opcion ingresada no es valida" );
-        }
-      } while( opc != 0 );
+	int opc;
+	int registro[4] = {0};
+    	Usuario lista[ MAX ][ MAX ];
+    	do{
+        	printf( "\nBienvenido al Banco de Sangre!\n" );
+        	printf( "Por favor, eliga su opcion\n" );
+        	printf( "1. Ingresar un usuario.\n" );
+        	printf( "2. Donar sangre.\n" );
+        	printf( "3. Mostrar donante.\n" );
+        	printf( "0. Salir.\n" );
+        	printf( "Su opcion: " );
+        	fflush( stdin );
+        	scanf( "%d" , &opc );
+        	switch( opc ){
+            		case 0:
+                		break;
+            		case 1:
+				agregarUsuario( lista, registro );
+                		break;
+            		case 2:
+                		donarSangre( lista );
+                		break;
+            		case 3:
+                		mostrarUsuario( lista );
+                		break;
+            		default:
+                		printf( "La opcion ingresada no es valida" );
+        	}
+      	} while( opc != 0 );
 }
--- bsangre.h
+++ bsangre.h
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAX 4
-#define SANMAX 1.5
 
 typedef struct Usuario{
 	char nombre[ 30 ], tipo[ 3 ];
@@ -13,19 +12,13 @@ typedef struct Usuario{
 void mostrarMenu();
 
 //Muestra un usuario específico del sistema, y lo hace preguntando el nombre
-void mostrarUsuario( Usuario lista[ MAX ][ MAX ] );
-
-//ejecuta el proceso para realizar un nuevo registro
-void nuevoUsuario( Usuario lista[  ][ MAX ], Usuario* pUsuario);
-
-//Devuelve 1 si hay un espacio disponible en el cual ingresar el usuario
-int verificarEspacio( Usuario lista[  ][ MAX ], int i );
-
-//Registra el struct llenado en nuevoUsuario en la matriz lista
-void guardarNuevoUsuario( Usuario lista[  ][ MAX ], Usuario* pUsuario, int i);
-
-//Devuelve el número que corresponde al tipo de sangre
-int numTipo( Usuario* pUsuario );
+void mostrarUsuario( Usuario lista[MAX][MAX] );
 
 //Aumenta la cantidad de sangre que el usuario ha donado
-void donarSangre(  Usuario lista[ MAX ][ MAX ] );
+void donarSangre(  Usuario lista[MAX][MAX] );
+
+//Agrega un nuevo usuario
+void agregarUsuario( Usuario lista[MAX][MAX], int registro[4] );
+
+//Guarda un usuario en los registros
+void guardarNuevoUsuario( Usuario lista[MAX][MAX], Usuario * pUsuario, int registro[4], int i );
